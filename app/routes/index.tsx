c201ec58be5059e1ae92f8b2ee0b1a07c2562e0e
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-01-22 18:27:28 -0300

    Initial countdown

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,22 @@
 export default function Index() {
+  const MILISEC_TO_DAYS: number = 1000 * 3600 * 24
+  const targetDate: Date = new Date('2022-4-10')
+  const today: Date = new Date()
+  const daysLeft: number = Math.floor((targetDate.getTime() - today.getTime()) / MILISEC_TO_DAYS)
+
+
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', maxWidth: 600, margin: '0 auto' }}>
+      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", textAlign: 'center', marginBottom: 30 }}>
+        <h1>{daysLeft} days left until game launch! &#127918;</h1>
+        <p>Target date: {targetDate.toLocaleDateString()}</p>
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, auto)', gap: 10, justifyContent: 'center' }}>
+        {[...Array(40)].map((x, i) =>
+          <div style={{ backgroundColor: 'rgba(255, 99, 71, 0.5)', width: 30, height: 30, borderRadius: 5 }} key={i} />
+        )}
+      </div>
     </div>
   );
 }
