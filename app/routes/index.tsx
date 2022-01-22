4f8a374e1a596f439393411aace9bd186f9c608a
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-01-22 18:40:07 -0300

    Install tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,18 +4,18 @@ export default function Index() {
   const today: Date = new Date()
   const daysLeft: number = Math.floor((targetDate.getTime() - today.getTime()) / MILISEC_TO_DAYS)
 
-
-
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', maxWidth: 600, margin: '0 auto' }}>
-      <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", textAlign: 'center', marginBottom: 30 }}>
-        <h1>{daysLeft} days left until game launch! &#127918;</h1>
-        <p>Target date: {targetDate.toLocaleDateString()}</p>
-      </div>
-      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, auto)', gap: 10, justifyContent: 'center' }}>
-        {[...Array(40)].map((x, i) =>
-          <div style={{ backgroundColor: 'rgba(255, 99, 71, 0.5)', width: 30, height: 30, borderRadius: 5 }} key={i} />
-        )}
+    <div style={{ width: '100%', height: '100vh', backgroundColor: 'rgba(242, 242, 242, 0.6)' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', maxWidth: 600, margin: '0 auto' }}>
+        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", textAlign: 'center', marginBottom: 30 }}>
+          <h1>{daysLeft} days left until game launch! &#127918;</h1>
+          <p>Target date: {targetDate.toLocaleDateString()}</p>
+        </div>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, auto)', gap: 10, justifyContent: 'center' }}>
+          {[...Array(40)].map((x, i) =>
+            <div style={{ backgroundColor: 'rgba(255, 99, 71, 0.5)', width: 30, height: 30, borderRadius: 5 }} key={i} />
+          )}
+        </div>
       </div>
     </div>
   );
