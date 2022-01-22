2a7db1a2a963fffb1289d3678815f68b78985539
Felipe Freitag Vargas <ffvargas@gmail.com>
2022-01-22 19:47:47 -0300

    Convert to Tailwind

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,15 +5,15 @@ export default function Index() {
   const daysLeft: number = Math.floor((targetDate.getTime() - today.getTime()) / MILISEC_TO_DAYS)
 
   return (
-    <div style={{ width: '100%', height: '100vh', backgroundColor: 'rgba(242, 242, 242, 0.6)' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', maxWidth: 600, margin: '0 auto' }}>
-        <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", textAlign: 'center', marginBottom: 30 }}>
-          <h1>{daysLeft} days left until game launch! &#127918;</h1>
-          <p>Target date: {targetDate.toLocaleDateString()}</p>
+    <div className="w-screen h-screen bg-slate-200">
+      <div className="max-w-md w-10/12 m-auto">
+        <div className="text-center py-14">
+          <h1 className="font-sans text-2xl mb-5">{daysLeft} days left until game launch! &#127918;</h1>
+          <h6 className="font-sans text-lg">Target date: {targetDate.toLocaleDateString()}</h6>
         </div>
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(7, auto)', gap: 10, justifyContent: 'center' }}>
+        <div className="grid grid-cols-7 gap-4 justify-items-center">
           {[...Array(40)].map((x, i) =>
-            <div style={{ backgroundColor: 'rgba(255, 99, 71, 0.5)', width: 30, height: 30, borderRadius: 5 }} key={i} />
+            <div className="rounded bg-red-300 w-8 h-8 self-center" key={i} />
           )}
         </div>
       </div>
